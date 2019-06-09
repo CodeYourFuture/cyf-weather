ea1a8ca6a9064133cb01dbdaa0254bbbc9ecf797
Samantha Chigbo <samchigbo@gmail.com>
2019-06-09 13:20:04 +0100

    adding log

--- src/App.js
+++ src/App.js
@@ -1,21 +1,20 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import svg from "./src/img/weather-icons/clear.svg";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-  render () {
+  render() {
     return (
       <div className="app">
-        <header className="app__header">
-        </header>
-        <main className="app__main">
-        </main>
+        <header className="app__header" />
+        hello
+        <img src={clear.svg} />
+        <main className="app__main" />
       </div>
     );
   }
