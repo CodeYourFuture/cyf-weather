7af34d8f4322ce8a8bc855bb127082f07ac296ae
Alisina92 <asb92.jobs@gmail.com>
2019-06-09 16:37:51 +0100

    button added

--- src/App.js
+++ src/App.js
@@ -12,9 +12,15 @@ class App extends Component {
       <div className="app">
         <header className="app__header">
           <h1>London</h1>
+
+          <p class="btn">FIND WEATHER</p>
         </header>
 
         <main className="app__main">
+          <img
+            src="https://i.colnect.net/b/1956/255/Winged-lion-in-gold.jpg"
+            alt="Persian stamp"
+          />
           <p>Temprature</p>
         </main>
       </div>
