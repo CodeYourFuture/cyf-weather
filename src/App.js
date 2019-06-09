fcb8c7c7b4f88696099a13ae2bbdcf581c1abe24
Q A <>
2019-06-09 13:18:49 +0100

    added hello message

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-  render () {
+  render() {
     return (
       <div className="app">
-        <header className="app__header">
-        </header>
-        <main className="app__main">
-        </main>
+        <header className="app__header" />
+        <h2>Say Hello To Todays Weather Mohbeen </h2>
+        <main className="app__main" />
       </div>
     );
   }
---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+<div>
+    <h1>Hello Todays Weather </h1> 
+</div>
+
+
+
+
+
+export default Hello;
