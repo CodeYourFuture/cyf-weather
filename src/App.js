6c1a2fccc0f290d6375101f407b035a2bc74ae40
najlasoltana <soltananajlaaa.gmail.com>
2019-06-09 12:55:44 +0100

    making sure the app is working

--- src/App.js
+++ src/App.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
-
   constructor(props) {
     super(props);
-    this.state = {
-    };
+    this.state = {};
   }
 
-  render () {
+  render() {
     return (
       <div className="app">
-        <header className="app__header">
-        </header>
-        <main className="app__main">
-        </main>
+        <header className="app__header" />
+        <main className="app__main">"Najla Soltana"</main>
       </div>
     );
   }
