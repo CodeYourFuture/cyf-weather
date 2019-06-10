1098de6cec9ee3eded57039e2e175577da169b83
Mussie Tecle <filago14@gmail.com>
2019-06-10 19:37:00 +0100

    another html/css look improvments

--- src/App.js
+++ src/App.js
@@ -15,7 +15,8 @@ class App extends Component {
     return (
       <div className="app">
         <header className="app-header">
-        <input className="inputBar"/>
+
+        <input placeholder="City" className="inputBar" />
         
           <button className="btn">FIND WEATHER</button>
         </header>
@@ -24,9 +25,55 @@ class App extends Component {
           <div className="main_pic"><img src="https://i.postimg.cc/2yF8LD0n/cloud-sun-copy-copy.png" className="main_pic_icon" /></div>
           <div className="overcast">Overcast Cloud</div>
           <div className="temp">Temprature 10°C to 11°C</div>
-          <div>
-            <div>Humidity  78%</div>
-            <div>Pressure 1008.48</div>
+          <div className="humidity_pressure">
+            <div>Humidity   <span className="digit">78%</span></div>
+            <div>Pressure   <span className="digit">1008.48</span></div>
+          </div>
+          <div className="twentyfour_hours">
+            <div>
+              <div className="time">03:00</div>
+              <div><img src="https://i.postimg.cc/2yF8LD0n/cloud-sun-copy-copy.png" className="icon" /></div>
+              <div className="degree">8°C</div>
+            </div>
+
+
+            <div>
+              <div className="time">06:00</div>
+              <div><img src="https://i.postimg.cc/2yF8LD0n/cloud-sun-copy-copy.png" className="icon" /></div>
+              <div className="degree">9°C</div>
+            </div>
+
+
+            <div>
+              <div className="time">09:00</div>
+              <div><img src="https://i.postimg.cc/28j0xkFZ/sun.png" className="icon" /></div>
+              <div className="degree">14°C</div>
+            </div>
+
+            <div>
+              <div className="time">12:00</div>
+              <div><img src="https://i.postimg.cc/28j0xkFZ/sun.png" className="icon" /></div>
+              <div className="degree">17°C</div>
+            </div>
+
+            <div>
+              <div className="time">15:00</div>
+              <div><img src="https://i.postimg.cc/28j0xkFZ/sun.png" className="icon" /></div>
+              <div className="degree">18°C</div>
+            </div>
+
+            <div>
+              <div className="time">18:00</div>
+              <div><img src="https://i.postimg.cc/28j0xkFZ/sun.png" className="icon" /></div>
+              <div className="degree">16°C</div>
+            </div>
+
+            <div>
+              <div className="time">21:00</div>
+              <div><img src="https://i.postimg.cc/2yF8LD0n/cloud-sun-copy-copy.png" className="icon" /></div>
+              <div className="degree">13°C</div>
+            </div>
+
           </div>
         </main>
       </div>
