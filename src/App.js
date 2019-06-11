29f2be67b0ec777ad5c1644ea839a33b631c659d
Q A <>
2019-06-11 16:20:07 +0100

    looks closer to the real thing now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import React, { Component } from "react";
 import "./App.css";
+import clear from "./img/weather-icons/clear.svg";
+import cloudy from "./img/weather-icons/cloudy.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
 
 class App extends Component {
   constructor(props) {
@@ -10,53 +17,73 @@ class App extends Component {
   render() {
     return (
       <div className="app">
-      <div className="container">
-        <header className="app__header">
-          <div className="searchbar">
-           <input className="Search"/>
-           <button>Find Weather</button>
-          </div>
-        </header>
-
-        <main className="app__main" >
-          <div className="Todays weather">
-            <div className ="current Weather">
-            <h4 className="temp" >Temperature</h4>
-            <div className="characteristics"> 
-            <h6 className="humidity ">Humidity</h6>
-            <h6 className="pressure">Pressure </h6>
+        <div className="container">
+          <header className="app__header">
+            <div className="searchbar">
+              <input className="Search" />
+              <button>Find Weather</button>
             </div>
-            </div>
-          </div> 
-        </main>
+          </header>
 
-        <span className="upcoming">
-         <ul>
-           <li>
-             <div className="time img temp">03:00</div>
-           </li>
-           <li>
-             <div className="time img temp">06:00</div>
-           </li>
-           <li>
-             <div className="time img temp">09:00</div>
-           </li>
-           <li>
-             <div className="time img temp">12:00</div>
-           </li>
-           <li>
-             <div className="time img temp">15:00</div>
-           </li>
-           <li>
-             <div className="time img temp">18:00</div>
-           </li>
-           <li>
-             <div className="time img temp">21:00</div>
-           </li>
-         </ul>
-        </span>
-    </div>
-    </div>
+          <main className="app__main">
+            <div className="Todays weather">
+              <div className="current Weather">
+                <img className="currentoverview" src={partlycloudy} />
+                <div className="overview">overcast clouds </div>
+                <h4 className="current temp">Temperature 10 to 11c</h4>
+                <div className="characteristics">
+                  <h6 className="humidity "> <b>Humidity </b>78%</h6>
+                  <h6 className="pressure"><strong>Pressure</strong> 1008.48></h6>
+                </div>
+              </div>
+            </div>
+
+            <div className="upcoming">
+              <div>
+                <div className="time">3:00 </div>
+                <img src={clear} />
+                <div className="degree">9c</div>
+              </div>
+
+              <div>
+                <div className="time">06:00</div>
+                <img src={cloudy} />
+                <div className="degree">9c</div>
+              </div>
+
+              <div>
+                <div className="time">09:00</div>
+                <img src={drizzle} />
+                <div className="degree">9c</div>
+              </div>
+
+              <div>
+                <div className="time">12:00</div>
+                <img src={fog} />
+                <div className="degree">9c</div>
+              </div>
+
+              <div>
+                <div className="time">15:00</div>
+                <img src={mostlycloudy} />
+                <div className="degree">9c</div>
+              </div>
+
+              <div>
+                <div className="time">18:00</div>
+                <img src={partlycloudy} />
+                <div className="degree">9c</div>
+              </div>
+
+              <div>
+                <div className="time">21:00</div>
+                <img src={rain} />
+                <div className="degree">9c</div>
+              </div>
+            </div>
+          </main>
+        </div>
+      </div>
     );
   }
 }
