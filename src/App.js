1ca3d3f14b8be6895cc564c1c7cca5d5761c48b0
Samantha Chigbo <samchigbo@gmail.com>
2019-06-12 18:09:56 +0100

    weather app with functionality more works on css and weather key functions

--- src/App.js
+++ src/App.js
@@ -1,50 +1,126 @@
-import React, { Component } from "react";
+import React from "react";
+import Weather from "./components/Weather";
+import Form from "./components/Form";
 import "./App.css";
+import clear from "./img/weather-icons/clear.svg";
 import cloudy from "./img/weather-icons/cloudy.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import fog from "./img/weather-icons/fog.svg";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import rain from "./img/weather-icons/rain.svg";
+import storm from "./img/weather-icons/storm.svg";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
+
+class App extends React.Component {
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  };
+
+  // i use getWeather is a method to make the api call
+  getWeather = async e => {
+    const city = e.target.elements.city.value;
+    e.preventDefault();
+    const api_call = await fetch(
+      `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${Api_Key}`
+    );
+    const response = await api_call.json();
+    console.log(response);
+    if (city) {
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        error: "Please input search values..."
+      });
+    }
+  };
 
   render() {
     return (
-      <div className="app">
-        <header className="app__header" />
-        <div className="flex-container">
-          <form onSubmit>
-            <input
-              type="text"
-              name="city"
-              placeholder="City..."
-              background-color="red"
-            />
-
-            <button className="weatherbutton">Find Weather!</button>
-          </form>
+      <div className="main-App">
+        <div className="form-container">
+          <Form loadWeather={this.getWeather} />
         </div>
-
-        <div />
-
-        <main />
-        <div className="App-main ">
-          <img src={cloudy} className="App-clear" align="bottom" />
+        <div className="weather-container">
+          <Weather
+            temperature={this.state.temperature}
+            city={this.state.city}
+            humidity={this.state.humidity}
+            description={this.state.description}
+            error={this.state.error}
+          />
+          <div className="weather-icons">
+            {" "}
+            <img src={clear} alt="clear" />
+            <p className="weather-type">overcast clear</p>
+          </div>
+          <div />
+          <div>
+            <h1>Tempreature:</h1>
+          </div>
+          <div>
+            <h1>Humidity:Pressure:</h1>
+          </div>
         </div>
-        <div>
-          <ul>
-            <li>03:00</li>
-            <li>06:00</li>
-            <li>09:00</li>
-            <li>12:00</li>
-            <li>15:00</li>
-            <li>18:00</li>
-            <li>21:00</li>
-          </ul>
+        <div className="weather-forcast">
+          <p>
+            03:00
+            <br />
+            <img src={cloudy} alt="cloudy" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
+          <p>
+            06:00 <br />
+            <img src={drizzle} alt="drizzle" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
+          <p>
+            09:00 <br />
+            <img src={fog} alt="fog" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
+          <p>
+            12:00 <br />
+            <img src={partlycloudy} alt="partlycloudy" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
+          <p>
+            15:00 <br />
+            <img src={rain} alt="rain" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
+          <p>
+            18:00 <br />
+            <img src={storm} alt="storm" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
+          <p>
+            21:00 <br />
+            <img src={storm} alt="storm" height="42" width="42" />
+            <br />
+            13&deg;C
+          </p>
         </div>
       </div>
     );
   }
 }
-
 export default App;
---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Form extends React.Component {
+  render() {
+    return (
+      <form onSubmit={this.props.loadWeather}>
+        <input type="text" name="city" placeholder="City..." />
+
+        <button>GET WEATHER</button>
+      </form>
+    );
+  }
+}
+
+export default Form;
