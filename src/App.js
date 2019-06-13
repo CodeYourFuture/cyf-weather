5482f133774d244cb484c92fb49cbb58ffce573a
Alisina92 <asb92.jobs@gmail.com>
2019-06-13 23:32:04 +0100

    getting started done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,12 @@
 import React, { Component } from "react";
+import partlycloudy from "./img/weather-icons/partlycloudy.svg";
+import clear from "./img/weather-icons/clear.svg";
+import mostlycloudy from "./img/weather-icons/mostlycloudy.svg";
+import storm from "./img/weather-icons/storm.svg";
+import drizzle from "./img/weather-icons/drizzle.svg";
+import rain from "./img/weather-icons/rain.svg";
+import snow from "./img/weather-icons/snow.svg";
+
 import "./App.css";
 import "./grid.css";
 
@@ -14,20 +22,22 @@ class App extends Component {
         <header className="app__header">
           <section class="search">
             <form action="">
-              <input class="text" placeholder="Search.." name="search" />
+              <input
+                class="text"
+                placeholder="Type in a city name..."
+                name="search"
+              />
               <button class="btn">FIND WEATHER</button>
             </form>
           </section>
         </header>
 
         <main className="app__main">
-          <img
-            src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Sunny.png"
-            alt="Sunny"
-          />
+          <img class="main-img" src={mostlycloudy} alt="mostly cloudy" />
+          <h3 class="title">overcast clouds</h3>
           <section class="flex-container1">
             <h3 class="temp">
-              <b>Temprature</b>&nbsp;&nbsp;&nbsp;10 to 11 C&nbsp;
+              <b>Temprature</b>&nbsp;&nbsp;&nbsp;10°to 11°C&nbsp;
             </h3>
 
             <h5 class="tempInfo">
@@ -42,79 +52,51 @@ class App extends Component {
             <div class="flex-box1">
               <p>03:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Snow_Occasional.png"
-                alt="SnowOcassional"
-              />
+              <img class="img-size" src={mostlycloudy} alt="mostly cloudy" />
               <br />
-              <p>8C</p>
+              <p>8°C</p>
             </div>
             <div class="flex-box2">
               <p>06:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Snow_Occasional.png"
-                alt="SnowOcassional"
-              />
+              <img class="img-size" src={mostlycloudy} alt="mostlyCloudy" />
               <br />
-              <p>9C</p>
+              <p>9°C</p>
             </div>
             <div class="flex-box3">
               <p>09:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Sunny.png"
-                alt="Sunny"
-              />
+              <img class="img-size" src={clear} alt="Sunny" />
               <br />
-              <p>14C</p>
+              <p>14°C</p>
             </div>
             <div class="flex-box4">
               <p>12:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Sunny.png"
-                alt="Sunny"
-              />
+              <img class="img-size" src={clear} alt="Sunny" />
               <br />
-              <p>17C</p>
+              <p>17°C</p>
             </div>
             <div class="flex-box5">
               <p>15:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Sunny.png"
-                alt="Sunny"
-              />
+              <img class="img-size" src={clear} alt="Sunny" />
               <br />
-              <p>18C</p>
+              <p>18°C</p>
             </div>
             <div class="flex-box6">
               <p>18:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Sunny.png"
-                alt="Sunny"
-              />
+              <img class="img-size" src={clear} alt="Sunny" />
               <br />
-              <p>16C</p>
+              <p>16°C</p>
             </div>
             <div class="flex-box7">
               <p>21:00</p>
               <br />
-              <img
-                class="img-size"
-                src="https://cdn4.iconfinder.com/data/icons/iconsland-weather/PNG/256x256/Snow_Occasional.png"
-                alt="SnowOcassional"
-              />
+              <img class="img-size" src={mostlycloudy} alt="mostlycloudy" />
               <br />
-              <p>13C</p>
+              <p>13°C</p>
             </div>
           </section>
         </main>
