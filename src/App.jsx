db71448c14768fa1e2101708667d7fa7f3e3a2bb
Sally McGrath <sally@codeyourfuture.io>
2023-01-30 19:51:29 +0000

    strip unwanted files, add missing test files

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.scss";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+
+//configs
+const siteTitle = process.env.REACT_APP_SITE_TITLE ?? "CYF Weather";
+
+function App() {
+  return (
+    <div className="App">
+      <Header title={siteTitle} />
+      <main className="c-site-main" tabIndex="0">
+      </main>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
