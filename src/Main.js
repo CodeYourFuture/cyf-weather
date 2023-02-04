f0709d851f54c77d8b941378872579e929665a22
Zaw Khing <zawmyokhing@gmail.com>
2023-02-04 12:16:20 +0000

    Create Main.js

---
+++ src/Main.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Main() {
+  return (
+    <div>
+        {/* fetch the data
+        images
+        current Weather
+        future Weather */}
+    </div>
+  )
+}
+
+export default Main
